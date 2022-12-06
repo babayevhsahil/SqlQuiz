a0643db35a9e85b119111a64b3664bda906171f9
Mirsail S. Babayev <mirsailsb@code.local>
2022-12-06 16:45:05 +0400

    SqlQuiz

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+Create Database CodeA
+
+
+Create Table Groups
+(
+ id int primary key,
+ Name nvarchar,
+
+ )
+
+ Create Table Students
+ (
+ Id int primary key,
+ Name nvarchar,
+ Surname nvarchar
+ )
+
